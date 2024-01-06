e706dc2b54e7b48aeacff560fd3612c849a5df35
yangding14 <122850418+yangding14@users.noreply.github.com>
2024-01-07 04:29:29 +0800

    update

--- app/src/main/java/com/example/eduempoweryd/MainActivity.java
+++ app/src/main/java/com/example/eduempoweryd/MainActivity.java
@@ -11,24 +11,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // if want skip login, uncomment this line and specify role (student or instructor) and uncomment the intent line below (line 17 and 18)
+//        skiplogin("instructor");
 
-        // TODO: change back to landing_page.class
-        String uid = "5bgKr3QuD6d3guVcHcD2FuNKc8q2";
-
-        SharedPreferences pref = getSharedPreferences("system", MODE_PRIVATE);
-        SharedPreferences.Editor editor = pref.edit();
-        editor.putString("uid", uid);
-        editor.apply();
-
-        // navigate to student
-//        Intent intent = new Intent(this, com.example.eduempoweryd.course.MainActivity.class);
-
-        // navigate to instructor
-        Intent intent = new Intent(this, com.example.eduempoweryd.course.InCourseList.class);
-
-
-//        Intent intent = new Intent(MainActivity.this, com.example.eduempoweryd.login.MainActivity.class);
+        Intent intent = new Intent(MainActivity.this, com.example.eduempoweryd.login.MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private void skiplogin(String role){
+        if(role.equals("student")){
+            String uid = "KW9dQocc6mUJtUkssl73sO07oDr2";
+
+            SharedPreferences pref = getSharedPreferences("system", MODE_PRIVATE);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putString("uid", uid);
+            editor.apply();
+
+            Intent intent = new Intent(this, com.example.eduempoweryd.course.MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else if(role.equals("instructor")){
+            String uid = "5bgKr3QuD6d3guVcHcD2FuNKc8q2";
+
+            SharedPreferences pref = getSharedPreferences("system", MODE_PRIVATE);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putString("uid", uid);
+            editor.apply();
+
+            Intent intent = new Intent(this, com.example.eduempoweryd.course.ListofCoursesActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 }
